0e3538371a49bfbd8166305913154211fa5c8883
alex91-html <alex91-html@users.noreply.github.com>
2025-05-29 18:47:15 +0200

    Set up basic Express API with endpoints for all thoughts, single thought, and API documentation

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 import cors from "cors"
 import express from "express"
+import listEndpoints from "express-list-endpoints";
+import data from "./data.json" assert { type: "json" }
+
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -11,11 +14,25 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-// Start defining your routes here
+// API documentation using express-list-endpoints
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!")
+  res.json(listEndpoints(app))
+})
+// Get all thoughts
+app.get("/thoughts", (req, res) => {
+  res.json(data)
 })
 
+// Get a single thought by _id
+app.get("/thoughts/:id", (req, res) => {
+  const { id } = req.params
+  const thought = data.find(t => t._id === id)
+  if (thought) {
+    res.json(thought)
+  } else {
+    res.status(404).json({ error: "Thought not found" })
+  }
+})
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
