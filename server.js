f91e12638162952d65d448078e0f0afb403acec7
alex91-html <alex91-html@users.noreply.github.com>
2025-06-06 21:23:08 +0200

    mongo URL'

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ import mongoose from "mongoose"
 // import dotenv from "dotenv"
 import Data from "./data.json"
 
+
+
+// the mongo URL to connect to the database
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost:27017/thoughts"
+
+
+
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
