35d33d291b92664fe65b58f27fbedca00ed59ae8
alex91-html <alex91-html@users.noreply.github.com>
2025-06-24 17:30:14 +0200

    implementing user.js with user model

---
+++ models/user.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3,
+    maxlength: 10
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  }
+});
+
+export const User = mongoose.model("User", userSchema);
+
